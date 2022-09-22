ed1a01673c2b8cdd4187aa4901567c04b3377442
Khayle <MKhayle@users.noreply.github.com>
2022-09-22 15:37:17 +0200

    Ajoutez des fichiers projet.

---
+++ API_Pokemon.cs
@@ -0,0 +1,39 @@
+﻿using PokeApiNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePokemon
+{
+    public class API_Pokemon
+    {
+        PokeApiClient pokeClient = new PokeApiClient();
+        List<Pokemon> ListePokemon = new List<Pokemon>();
+
+
+        public async void API_FetchAll()
+        {
+            for (int i = 1; i <= 151; i++)
+            {
+                if (i == 50 || i == 100)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                }
+                Pokemon current = await pokeClient.GetResourceAsync<Pokemon>(i);
+                ListePokemon.Add(current);
+                if (ListePokemon[i-1].Types.Count() == 1)
+                {
+                    Console.WriteLine($"#{ListePokemon[i-1].Id} : {ListePokemon[i - 1].Species.Name} ( {ListePokemon[i-1].Types[0].Type.Name} )");
+                }
+                else
+                {
+                    Console.WriteLine($"#{ListePokemon[i - 1].Id} : {ListePokemon[i - 1].Species.Name} ( {ListePokemon[i - 1].Types[0].Type.Name} / {ListePokemon[i - 1].Types[1].Type.Name} )");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using ConsolePokemon;
+using System.Text;
+
+internal class Programm
+{
+    // Main de l'application pokédex
+
+    static void Main(String[] args)
+    {
+
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.Write("\nApplication pokédex - Menu principal");
+            Console.Write("\nCréer une page pour un seul pokémon : 1");
+            Console.Write("\nIndiquer une liste de pokémon à afficher : 2");
+            Console.Write("\nFaire une recherche sur le site pokemontruc : 3");
+            Console.Write("\nQuitter : 0");
+            Console.Write("\nEntrez une commande :");
+            Console.WriteLine();
+
+
+            string param = Console.ReadLine() ?? "";
+
+            switch (param)
+            {
+                case "1":
+                    ConsolePokemon.HTML_Handler.CreerHTML();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Hello, World!");
+                    API_Pokemon API = new API_Pokemon();
+                    API.API_FetchAll();
+
+                    Console.WriteLine("Récupération depuis l'API en cours...");
+                    System.Threading.Thread.Sleep(15000);
+                    Console.ReadKey();
+
+                    break;
+
+                case "3":
+                    break;
+
+                case "0":
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("\nCommande non reconnnue");
+                    break;
+            }
+        }
+    }
+}
